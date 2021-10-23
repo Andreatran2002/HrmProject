6f3387af893f5b05e6176ab10a07ca41e6496a98
andreatran2002 <andreatran2002@gmail.com>
2021-10-23 13:29:17 +0700

    update avatar in user

--- Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -1,9 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using App.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -15,15 +18,19 @@ namespace hrmProject.Areas.Identity.Pages.Account.Manage
         private readonly UserManager<AppUser> _userManager;
         private readonly SignInManager<AppUser> _signInManager;
 
+        private IHostingEnvironment _environment;
         public IndexModel(
             UserManager<AppUser> userManager,
-            SignInManager<AppUser> signInManager)
+            SignInManager<AppUser> signInManager, 
+            IHostingEnvironment environment)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _environment = environment;
         }
 
         public string Username { get; set; }
+        public IFormFile Avatar{set;get;}
 
         [TempData]
         public string StatusMessage { get; set; }
@@ -31,11 +38,27 @@ namespace hrmProject.Areas.Identity.Pages.Account.Manage
         [BindProperty]
         public InputModel Input { get; set; }
 
+
         public class InputModel
         {
             [Phone]
             [Display(Name = "Phone number")]
             public string PhoneNumber { get; set; }
+            [Display(Name = "Địa chỉ thường trú")]
+            public string HomeAddress { get; set; }
+
+            [Display(Name = "Quê quán")]
+            public string Hometown { get; set; }
+
+            [Display(Name = "Giói tính")]
+            public string Sex { get; set; }
+
+            [Display(Name = "Tôn giáo")]
+            public string Religion { get; set; }
+
+            [Display(Name = "Ngày sinh")]
+            public DateTime? BirthDate { get; set; }
+            
         }
 
         private async Task LoadAsync(AppUser user)
@@ -47,7 +70,15 @@ namespace hrmProject.Areas.Identity.Pages.Account.Manage
 
             Input = new InputModel
             {
-                PhoneNumber = phoneNumber
+                PhoneNumber = phoneNumber,
+                HomeAddress = user.HomeAddress,
+                BirthDate = user.BirthDate,
+                Hometown = user.Hometown,
+                Sex= user.Sex,
+                Religion = user.Religion,
+                
+
+
             };
         }
 
@@ -58,14 +89,16 @@ namespace hrmProject.Areas.Identity.Pages.Account.Manage
             {
                 return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
             }
-
+            if (user.Avatar != null)avatar_file = "/uploads/" + user.Avatar;
             await LoadAsync(user);
             return Page();
         }
+        public AppUser user{set;get;}
+        public string avatar_file { get; set; }
 
         public async Task<IActionResult> OnPostAsync()
         {
-            var user = await _userManager.GetUserAsync(User);
+            user = await _userManager.GetUserAsync(User);
             if (user == null)
             {
                 return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
@@ -78,16 +111,31 @@ namespace hrmProject.Areas.Identity.Pages.Account.Manage
             }
 
             var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
-            if (Input.PhoneNumber != phoneNumber)
-            {
-                var setPhoneResult = await _userManager.SetPhoneNumberAsync(user, Input.PhoneNumber);
-                if (!setPhoneResult.Succeeded)
-                {
-                    StatusMessage = "Unexpected error when trying to set phone number.";
-                    return RedirectToPage();
+            // if (Input.PhoneNumber != phoneNumber)
+            // {
+            //     var setPhoneResult = await _userManager.SetPhoneNumberAsync(user, Input.PhoneNumber);
+            //     if (!setPhoneResult.Succeeded)
+            //     {
+            //         StatusMessage = "Unexpected error when trying to set phone number.";
+            //         return RedirectToPage();
+            //     }
+            // }
+            user.HomeAddress = Input.HomeAddress;
+            user.PhoneNumber = Input.PhoneNumber; 
+            user.BirthDate = Input.BirthDate; 
+            user.Hometown = Input.Hometown;
+            user.Sex = Input.Sex; 
+            user.Religion = Input.Religion; 
+
+
+            if (Avatar != null) {
+                var file = Path.Combine (_environment.ContentRootPath, "wwwroot/uploads", Avatar.FileName);
+                using (var fileStream = new FileStream (file, FileMode.Create)) {
+                await Avatar.CopyToAsync (fileStream);
+                user.Avatar=Avatar.FileName; 
                 }
             }
-
+            await _userManager.UpdateAsync(user); 
             await _signInManager.RefreshSignInAsync(user);
             StatusMessage = "Your profile has been updated";
             return RedirectToPage();
